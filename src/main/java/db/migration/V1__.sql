709c64f120ecb942aae4204dffdd47afabe0c501
ssa <serega.sheypak@gmail.com>
2015-06-22 11:34:04 +0200

    try flayaway

---
+++ src/main/java/db/migration/V1__.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS unique_visitor
+(
+   visitorId VARCHAR   NOT NULL,
+   siteId    INTEGER   NOT NULL,
+   visitTs   BIGINT
+   CONSTRAINT my_pk  PRIMARY KEY (visitorId, siteId)
+)
